ebe6d81d474b457e7ec0c06ce1db3f331a8d1bfe
RhondaStark92 <rhondastark@comcast.net>
2018-11-19 16:04:09 -0600

    added material ui for lists and reworked the original fetch to join with item

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -6,7 +6,10 @@ const router = express.Router();
 
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query(`SELECT * FROM "shopping_list" WHERE person_id = ${req.user.id}`)
+    
+    pool.query(`SELECT shopping_list.*, item.name as item, item.brand_name 
+                FROM "shopping_list" JOIN "item" ON shopping_list.item_id = item.id
+                WHERE shopping_list.person_id = ${req.user.id}`)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for shopping list:', error);
--- src/components/ShoppingList/ShoppingList.js
+++ src/components/ShoppingList/ShoppingList.js
@@ -1,8 +1,17 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
 
 class ShoppingList extends Component {
   
+  state = {
+    checked: [0],
+  };
+
   componentDidMount () {
     this.props.dispatch({type: 'FETCH_LIST'})
     console.log('in shopping list did mount', this.props.list);    
@@ -10,8 +19,28 @@ class ShoppingList extends Component {
 
   render() {
     return (
-      <h1>Shopping List</h1>
-      // <pre>JSON.stringify({this.props.reduxState.list})</pre>
+      <div>
+        <h1>Shopping List</h1>
+        {/* <pre>{JSON.stringify(this.props.list)}</pre> */}
+        <List>
+          {this.props.list.map(item => (
+            <ListItem key={item.id} role={undefined} dense button>
+              <Checkbox
+                checked={this.state.checked.indexOf(item) !== -1}
+                tabIndex={-1}
+                disableRipple
+              />
+              <ListItemText primary={item.item}  />
+              <ListItemSecondaryAction>
+                {/* <IconButton aria-label="Comments">
+                  <CommentIcon />
+                </IconButton> */}
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
+
+      </div>
     )}
 }
 // Instead of taking everything from state, we just want the list info.
