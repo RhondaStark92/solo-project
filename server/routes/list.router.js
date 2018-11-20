5f5474ff8cec5fc3135a6c072a9e99a53732b27e
RhondaStark92 <rhondastark@comcast.net>
2018-11-19 21:42:42 -0600

    updated with styled components

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -9,7 +9,8 @@ router.get('/', rejectUnauthenticated, (req, res) => {
     
     pool.query(`SELECT shopping_list.*, item.name as item, item.brand_name 
                 FROM "shopping_list" JOIN "item" ON shopping_list.item_id = item.id
-                WHERE shopping_list.person_id = ${req.user.id}`)
+                WHERE shopping_list.person_id = ${req.user.id}
+                ORDER BY shopping_list.found`)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for shopping list:', error);
