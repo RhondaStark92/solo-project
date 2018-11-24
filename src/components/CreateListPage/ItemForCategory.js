107a8a625a8f0de702cf6489fe6831fd69f15924
RhondaStark92 <rhondastark@comcast.net>
2018-11-24 11:04:09 -0600

    add badge for quantity if on the shopping list and added delete button

--- src/components/CreateListPage/ItemForCategory.js
+++ src/components/CreateListPage/ItemForCategory.js
@@ -3,11 +3,12 @@ import {connect} from 'react-redux';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import Checkbox from '@material-ui/core/Checkbox';
 import DeleteIcon from '@material-ui/icons/Delete';
-import Button from '@material-ui/core/Button';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -23,43 +24,54 @@ const styles = theme => ({
 class ItemForCategory extends Component {
   
   handleItemClick = item => () => {
-    console.log('clicked on item to add to list', item);
+    console.log('add item to list', item);
+    // this.props.dispatch({ type: 'FOUND_ITEM', payload: {item: item, store_id: this.props.store_id}})
+  };
+
+  handleDeleteClick = item => () => {
+    console.log('delete item', item);
     // this.props.dispatch({ type: 'FOUND_ITEM', payload: {item: item, store_id: this.props.store_id}})
   };
 
   render() {
 
     let itemOnList = true;
-    console.log('quantity', this.props.itemForCat.quantity);
-    
     if (this.props.itemForCat.quantity === null) {
       itemOnList = false;
     }
     
     return (
       <ListItem key={this.props.itemForCat.item_id} divider={true} dense={true}
-          role={undefined} button onClick={this.handleItemClick(this.props.itemForCat.item_id)} >
+          button onClick={this.handleItemClick(this.props.itemForCat.item_id)} >
           <Checkbox
               checked={itemOnList}
               tabIndex={-1}
               disableRipple
             />
         <Typography>
+        <Badge color="secondary" badgeContent={this.props.itemForCat.quantity} invisible={!itemOnList} >              
           <ListItemText>
               {this.props.itemForCat.item}
           </ListItemText>
+          </Badge>
         </Typography>
         <ListItemSecondaryAction>
           <div>
-            {this.props.itemForCat.quantity}
-            <Button>X</Button>
+            {/* <IconButton aria-label="Add">
+              <AddCircleIcon />
+            </IconButton> */}
+            <IconButton onClick={this.handleDeleteClick(this.props.itemForCat.item_id)} 
+              aria-label="Delete">
+              <DeleteIcon />
+            </IconButton>
+
           </div>
         </ListItemSecondaryAction>
       </ListItem>
     )}
 }
 
-// Instead of taking everything from state, we just want the list info.
+// Instead of taking everything from state, we just want the item info.
 // if you wanted you could write this code like this:
 // const mapStateToProps = ({user}) => ({ user });
 const mapStateToProps = state => ({
